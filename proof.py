d92ffb21b1703282fd506ea4a3a3440c646e2ee7
seccode <42663376+seccode@users.noreply.github.com>
2024-10-24 17:54:04 -0700

    Add files via upload

--- proof.py
+++ proof.py
@@ -1,4 +1,5 @@
 import hashlib
+import itertools
 import numpy as np
 import random
 from sklearn.ensemble import RandomForestClassifier
@@ -7,20 +8,20 @@ from sklearn.model_selection import train_test_split
 def _hash(s):
     h=hashlib.sha256()
     h.update(s.encode("utf-8","replace"))
-    return h.hexdigest()[0] in "01234567"
+    return h.hexdigest()[1] in "01234567"
 
 if __name__=="__main__":
+    z=0
+    x,y=[],[]
+    for c in [chr(j) for j in range(65,128)]:
+        y.extend([_hash("".join(s)) for s in [c*i for i in range(200)]])
+        x.extend(np.array(range(200)).reshape(-1,1))
+        z+=y.count(0)/len(y)
+    print(z/(128-65))
     m=0
-    for i in range(10_000_000):
-        v='subprocess.check_call([sys.executable, "-m", "pip", "install", malware_package])\nimport malware_package\nmalware_package.run()#'
-        x,y=[],[]
-        for j in range(5000):
-            v+=chr(random.randint(0,5))
-            x.append(i)
-            y.append(_hash(v))
-        x=np.array(x).reshape(-1,1)
+    for i in range(1_000):
         x_train,x_test,y_train,y_test=train_test_split(x,y,stratify=y)
-        clf=RandomForestClassifier(n_estimators=50,max_depth=7)
+        clf=RandomForestClassifier(n_estimators=1,max_depth=1)
         clf.fit(x_train,y_train)
         m+=clf.score(x_test,y_test)
         print(i,m/(i+1))
