dfd418a4a6c07a938b50f35105cfb99e6da30e47
seccode <42663376+seccode@users.noreply.github.com>
2024-10-24 18:05:28 -0700

    Add files via upload

--- proof.py
+++ proof.py
@@ -14,13 +14,14 @@ if __name__=="__main__":
     z=0
     x,y=[],[]
     for c in [chr(j) for j in range(65,128)]:
+        x.extend(list(range(len(x),len(x)+200)))
         y.extend([_hash("".join(s)) for s in [c*i for i in range(200)]])
-        x.extend(np.array(range(200)).reshape(-1,1))
         z+=y.count(0)/len(y)
+    x=np.array(x).reshape(-1,1)
     print(z/(128-65))
     m=0
-    for i in range(1_000):
-        x_train,x_test,y_train,y_test=train_test_split(x,y,stratify=y)
+    for i in range(100_000):
+        x_train,x_test,y_train,y_test=train_test_split(x,y,stratify=y,test_size=0.2)
         clf=RandomForestClassifier(n_estimators=1,max_depth=1)
         clf.fit(x_train,y_train)
         m+=clf.score(x_test,y_test)
