519489f221e8bf2400044c9eb0d732cef5f93527
seccode <42663376+seccode@users.noreply.github.com>
2024-10-24 18:15:32 -0700

    Add files via upload

--- proof.py
+++ proof.py
@@ -4,6 +4,7 @@ import numpy as np
 import random
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from tqdm import tqdm
 
 def _hash(s):
     h=hashlib.sha256()
@@ -11,14 +12,15 @@ def _hash(s):
     return h.hexdigest()[1] in "01234567"
 
 if __name__=="__main__":
-    z=0
     x,y=[],[]
-    for c in [chr(j) for j in range(65,128)]:
-        x.extend(list(range(len(x),len(x)+200)))
-        y.extend([_hash("".join(s)) for s in [c*i for i in range(200)]])
-        z+=y.count(0)/len(y)
+    for c in tqdm([chr(j) for j in range(10_000)]):
+        x.extend(list(range(len(x),len(x)+1_000)))
+        z=c
+        for i in range(1_000):
+            y.append(_hash(z))
+            z+=c
     x=np.array(x).reshape(-1,1)
-    print(z/(128-65))
+    print(y.count(1)/len(y))
     m=0
     for i in range(100_000):
         x_train,x_test,y_train,y_test=train_test_split(x,y,stratify=y,test_size=0.2)
